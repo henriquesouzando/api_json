80fef1c8560b7eefe268016a151faefb8f8d9dc1
Henrique de Souza Ando <Henrique.sando@corp.medgrupo.net>
2021-11-10 17:40:24 -0300

    Implementado dois módulos de API, modulo def_api, é a api desenvolvida em função, já o módulo class_api, foi desenvolvido em classe.

---
+++ class_api.py
@@ -0,0 +1,43 @@
+﻿from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
+# criando o app flask
+app = Flask(__name__)
+# criando objeto api
+api = Api(app)
+  
+#recurso de envio e recebimento de dados
+class Hello(Resource):
+  
+    
+    # metodo get 
+    def get(self):
+  
+        return jsonify({'message': "Hello tech recruiter! Welcome my api in class."})
+  
+    # metodo post 
+    def post(self):
+          
+        data = request.get_json()  
+        
+        # status code
+        return jsonify(data, 201)
+  
+  
+# Recurso de raiz quadrada
+class Square(Resource):
+  
+    def get(self, num):
+  
+        return jsonify({'square': num**2})
+  
+  
+# adicionando os recursos definidos junto com suas urls correspondentes
+api.add_resource(Hello, '/')
+api.add_resource(Square, '/square/<int:num>')
+  
+  
+
+if __name__ == '__main__':
+  
+    app.run(debug = True)
---
+++ def_api.py
@@ -0,0 +1,36 @@
+﻿# Using flask to make an api
+# import necessary libraries and functions
+from flask import Flask, jsonify, request
+  
+# creating a Flask app
+app = Flask(__name__)
+  
+# No terminal digitamos o comando 'curl' mais a url (http://127.0.0.1:5000/)
+# Teremos como response uma mensagem de boas vindas.
+# diferença entre get e post, no get a requisição é feita via URL e tem limite de caracteres que é 255, já no post não limite de caracteres, pois a mensagem é enviada junto com o corpo da requisição http. 
+# Nessa função temos apenas o método get disponível 
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+  
+        data = "Hello tech recruiter! Welcome my api in function."
+        return jsonify({'data': data})
+  
+# Um simple função para calcular a raiz quadrada
+# O numero pode ser enviado pela url com método GET
+# No terminal podemos testar utilizando o comando curl e a url (http://127.0.0.1:5000/square/10)
+# O retorno será no formato json
+
+@app.route('/square/<int:num>', methods = ['GET'])
+def disp(num):
+  
+    return jsonify({'data': num**2})
+  
+
+# driver function
+if __name__ == '__main__':
+  
+    app.run(debug = True)
+
+
